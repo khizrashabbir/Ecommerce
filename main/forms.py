c88d965e6e11e11c051be6a10a308abb5bd8e93a
khizrashabbir <khizrashabbir3@gmail.com>
2022-04-19 13:15:55 +0500

    review user hidden

--- main/forms.py
+++ main/forms.py
@@ -8,11 +8,17 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['name', 'user','title', 'review','rating']
+        fields = ['name', 'user','title', 'product','review','rating']
+
+        # def __init__(self, *args, **kwargs):
+        #     super().__init__(*args, **kwargs)
+        #     self.fields['user'].widget.attrs.update({'class': 'form-control '})
         # comment = forms.CharField(widget=forms.Textarea)
         widgets = {
                 'name': forms.TextInput(attrs={'class': 'form-control'}),
-                'user': forms.Select(),
+                # 'user': forms.Select(attrs={'readonly'}),
+                'user': forms.Select(attrs={'class': 'hidden'}),
+                'product': forms.Select(attrs={'class': 'hidden'}),
                 'title': forms.TextInput(attrs={'class':'form-control'}),
                 'review': forms.Textarea(attrs={'class':'form-control'}),
                 'rating': forms.NumberInput(attrs={'class': 'rating rating-selection'}),
